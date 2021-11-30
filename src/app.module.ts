4a9d183a7f7687f1826f9b13853c008aca38aa36
chenxinhu <xingshi8000@gmail.com>
2021-11-30 18:36:55 +0800

    add neo4j and neo4j graphql library support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import neo4j from 'neo4j-driver';
+import { Neo4jGraphQL } from '@neo4j/graphql';
+import { readFileSync } from 'fs';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRootAsync({
+      useFactory: async () => {
+        const typeDefs = readFileSync('schema/schema.graphql', 'utf-8');
+
+        const driver = neo4j.driver(
+          process.env.NEO4J_SERVER,
+          neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
+        );
+
+        const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+        return {
+          debug: true,
+          playground: true,
+          schema: neoSchema.schema,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
